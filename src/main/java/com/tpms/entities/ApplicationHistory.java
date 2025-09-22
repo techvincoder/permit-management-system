ff73f554c1f2aba902b654601a4b000b9ef7cf54
silverdagger <pravinyadav1604@gmail.com>
2025-09-23 01:25:05 +0530

    fead: there was a bug in DB

--- src/main/java/com/tpms/entities/ApplicationHistory.java
+++ src/main/java/com/tpms/entities/ApplicationHistory.java
@@ -10,7 +10,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name ="table_history")
+@Table(name ="application_history")
 public class ApplicationHistory {
 
 	@Id
